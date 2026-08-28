747cfaa498ee8e27d09900f8b38b712b77cec952
Muis Sinanda <muis.sinanda@gmail.com>
2026-08-28 12:13:41 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-﻿from fastapi import FastAPI, Request, Form, Depends, HTTPException
+from fastapi import FastAPI, Request, Form, Depends, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse, HTMLResponse, StreamingResponse
@@ -23,38 +23,38 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def dashboard(request: Request, username: str = Depends(verify_credentials)):
-    return templates.TemplateResponse("index.html", {"request": request, "channels": manager.get_all_channels()})
+    return templates.TemplateResponse(request=request, name="index.html", context={"channels": manager.get_all_channels()})
 
 @app.post("/add")
 async def add_channel(name: str = Form(...), input_url: str = Form(...), username: str = Depends(verify_credentials)):
     manager.add_channel(name, input_url)
-    return RedirectResponse("/", status_code=303)
+    return RedirectResponse(url="/", status_code=303)
 
 @app.post("/delete/{channel_id}")
 async def delete_channel(channel_id: str, username: str = Depends(verify_credentials)):
     manager.delete_channel(channel_id)
-    return RedirectResponse("/", status_code=303)
+    return RedirectResponse(url="/", status_code=303)
 
 @app.post("/start/{channel_id}")
 async def start_channel(channel_id: str, username: str = Depends(verify_credentials)):
     manager.start_stream(channel_id)
-    return RedirectResponse("/", status_code=303)
+    return RedirectResponse(url="/", status_code=303)
 
 @app.post("/stop/{channel_id}")
 async def stop_channel(channel_id: str, username: str = Depends(verify_credentials)):
     manager.stop_stream(channel_id)
-    return RedirectResponse("/", status_code=303)
+    return RedirectResponse(url="/", status_code=303)
 
 @app.post("/restart/{channel_id}")
 async def restart_channel(channel_id: str, username: str = Depends(verify_credentials)):
     manager.restart_stream(channel_id)
-    return RedirectResponse("/", status_code=303)
+    return RedirectResponse(url="/", status_code=303)
 
 @app.get("/play/{channel_id}", response_class=HTMLResponse)
 async def play_channel(request: Request, channel_id: str):
     channel = manager.get_channel(channel_id)
     if not channel: raise HTTPException(status_code=404)
-    return templates.TemplateResponse("player.html", {"request": request, "channel": channel})
+    return templates.TemplateResponse(request=request, name="player.html", context={"channel": channel})
 
 @app.get("/stream/{channel_id}")
 async def stream_video(request: Request, channel_id: str):
